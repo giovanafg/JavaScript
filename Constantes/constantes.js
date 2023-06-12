0c81cc045766848c266f8598da683dac76d4b28b
Giovana <giovanaworkfg@gmail.com>
2023-06-12 19:01:52 -0300

    Constantes

---
+++ Constantes/constantes.js
@@ -0,0 +1,8 @@
+//No momento em que uma constante recebe um determinado valor, esse não pode ser redefinido ao longo do código
+
+/*Construção de uma constante:
+  Tipo da constante, nome da constante, valor*/  
+  const nomeConstante = 10;
+
+//Executando no console
+console.log(nomeConstante);
--- Variáveis/variaveis.js
+++ Variáveis/variaveis.js
@@ -2,11 +2,9 @@
 
 /*Construção de uma variável:
   Tipo da variável, nome da variável, valor*/
-
-let nomeVariavel = 0;
+  let nomeVariavel = 0;
 
 //O valor de uma variável pode mudar, ao longo do código
 
 //Executando no console
-
-console.log(nomeVariavel)
+  console.log(nomeVariavel)
