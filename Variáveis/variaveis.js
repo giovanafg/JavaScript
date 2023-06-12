b18dc6feb18528f820472a73c184eb937adbe403
Giovana <giovanaworkfg@gmail.com>
2023-06-12 18:41:32 -0300

    Variáveis

---
+++ Variáveis/variaveis.js
@@ -0,0 +1,12 @@
+//Declarando uma variável
+
+/*Construção de uma variável:
+  Tipo da variável, nome da variável, valor*/
+
+let nomeVariavel = 0;
+
+//O valor de uma variável pode mudar, ao longo do código
+
+//Executando no console
+
+console.log(nomeVariavel)
